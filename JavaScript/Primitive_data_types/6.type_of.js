aec7a466fbd2a78a12ea78f8c158e2da6cff5633
Rwaa Assi <rwaaassi123@gmail.com>
2024-04-14 16:34:35 +0300

    typeof

---
+++ JavaScript/Primitive_data_types/6.type_of.js
@@ -0,0 +1,27 @@
+// 1
+let num = 10;
+let str = "hello";
+let isbool = true;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof isbool);
+
+// 2
+let firstVar = 42;
+let secondVar = "42";
+console.log(typeof firstVar);
+console.log(typeof secondVar);
+
+// 3
+const noValue = undefined;
+const emptyValue = null;
+console.log(noValue);
+console.log(emptyValue);
+
+//4
+let x;
+const y = 10;
+console.log(typeof y);
+x = "The value of x is " + y + " and its type is " + typeof y;
+console.log(x);
