f19527782b9325fec77ad56c4c13804294f63b7d
Rwaa Assi <rwaaassi123@gmail.com>
2024-04-14 15:22:40 +0300

    literals

---
+++ JavaScript/Primitive_data_types/5.template_literals.js
@@ -0,0 +1,32 @@
+let x;
+const name = "Rwaa";
+const age = 21;
+x = "Hello my name is " + name + " And i am " + age + " years old";
+console.log(x);
+
+let y;
+const length = 5;
+const width = 7;
+ area = length * width 
+y = "The area of a rectangle with length of " + length + "and width of " + width + " is " + area;
+console.log(y);
+
+let z;
+const day = "Sunday";
+const task1 = "study for the exam";
+const task2 = "do the dishes";
+const task3 = "submit the assignment";
+z = "On " + day + ", I will " + task1 + ", " + task2 + ", and " + task3;
+console.log(z);
+
+let pets;
+const number = 2;
+pets = "I have " + number + " pets."
+console.log(pets);
+
+let h 
+const base = 4
+const height = 6
+area = (base * height) / 2
+h = "The area of a triangle with base " + base + " and " + height + " is " + area;
+console.log(h);
