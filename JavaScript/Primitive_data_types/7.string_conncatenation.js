6e2596094633924433e7521eea6ab31ae82e401b
Rwaa Assi <rwaaassi123@gmail.com>
2024-04-15 10:33:24 +0300

    string concatenation

---
+++ JavaScript/Primitive_data_types/7.string_conncatenation.js
@@ -0,0 +1,20 @@
+const name = "Rwaa";
+const Age = 21;
+console.log(`My name is ${name} and I am ${Age}  years old.`);
+
+const fruit = "peach";
+const color = "green";
+const movie = "Decision to Leave";
+
+console.log(
+  `my favorite color is ${color}, I love the movie ${movie}, and my favorite fruit is ${fruit} `
+);
+
+const a = 10;
+const b = 5;
+console.log(
+  `the sum of a and b is ${a + b}, ${a + b} is the sum of ${a} and ${b}`
+);
+
+const day = "Monday";
+console.log(`today is ${day}`);
