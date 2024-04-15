afab2ee110a931bf2dfc4663e4ba0fd261fcf095
Rwaa Assi <rwaaassi123@gmail.com>
2024-04-15 03:27:33 +0300

    8+9

---
+++ JavaScript/Basic_operators/9.boolean_expressions.js
@@ -0,0 +1,49 @@
+// 1
+const isSunny = true;
+const isWeekend = false;
+console.log(isSunny && isWeekend);
+
+// 2
+const age = 20
+const license = true
+console.log( age> 18 )
+
+// 3
+const knowsJavaScript = false
+const knowsPython = true
+console.log(knowsJavaScript || knowsPython);
+
+// 4
+const isRaining = true
+console.log(!isRaining);
+
+// 5
+const likeMusic = true
+const playsGuitar = false
+const hasTime = true
+console.log(likeMusic || playsGuitar && hasTime);
+
+// 6
+const personAge = 25
+console.log( personAge > 18 );
+
+// 7
+const currentTemperature = 34
+console.log( currentTemperature<20 , currentTemperature>30);
+
+// 8
+const firstNumber = 10
+const secondNumber = 10
+console.log(firstNumber == secondNumber , firstNumber===secondNumber);
+
+// 9
+const budget = 500
+const expense = 450
+console.log(expense<=budget);
+
+// 10
+const person1Height = 175
+const person2Height = 160
+console.log(person1Height> person2Height , person1Height<person2Height, person1Height==person2Height);
+
+
---
+++ JavaScript/Basic_operators/Basic Arithmetic.js
@@ -0,0 +1,54 @@
+// 1
+const num1 = 8;
+const num2 = 15;
+console.log(num1 + num2);
+
+// 2
+const a = 30;
+const b = 12;
+console.log(a - b);
+
+// 3
+const x = 7;
+const y = 3;
+console.log(x * y);
+
+// 4
+const n1 = 20;
+const n2 = 4;
+console.log(20 % 4);
+
+// 5
+const number1 = 15;
+const number2 = 25;
+const number3 = 10;
+console.log((number1 + number2 + number3) / 3);
+
+// 6
+// the modulo operator claculates the remainder of a dividing equation. and it finds if the value is odd or even
+const number5 = 14;
+console.log(number5 % 3);
+
+// 7
+const n5 = 10;
+console.log(n5 % 2, n5 % 2 == 0);
+
+// 8
+const oddEven = 125;
+console.log(oddEven % 5, oddEven % 7, (oddEven % 5) + (oddEven % 7) == 0);
+
+// // 9
+// const max = 1, 20
+// console.log(max%4);
+
+// 10
+const num6 = 19;
+console.log(num6 > 10 && num6 < 20);
+
+// 11
+const num7 = -6;
+console.log(num7 < -5 || num7 > 10);
+
+// 12
+const num8 = 44;
+console.log((num8 > 5 && num8 < 10) || (num8 > 20 && num8 < 30));
