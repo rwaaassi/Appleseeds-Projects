224b4fbf539c0975eea2e200165e7fe994ddf92f
Rwaa Assi <rwaaassi123@gmail.com>
2024-05-06 18:15:14 +0300

    objects

--- JavaScript/objects/objects.js
+++ JavaScript/objects/objects.js
@@ -1,16 +1,4 @@
 // 1
-// const movieDatabase = {
-//     movies : [{title: "",
-//     ratings: [],
-//     averageRating: ,
-//     }],
-//     addMovie = function ("past lives") {
-
-//     }
-// }
-
-// 1 new
-// 1
 const movieDatabase = {
   movies: [],
   addMovie: function (movieTitle) {
@@ -31,38 +19,114 @@ const movieDatabase = {
 };
 
 movieDatabase.addMovie("past lives");
-movieDatabase.addMovie("kkkk");
-movieDatabase.addMovie("ddddd");
-console.log(movieDatabase);
+movieDatabase.addMovie("portrait of a lady on fire");
+movieDatabase.addMovie("decision to leave");
+movieDatabase.addMovie("lady bird");
 
-movieDatabase.removeMovie("kkkk");
-console.log(movieDatabase);
+// console.log(movieDatabase);
+
+movieDatabase.removeMovie("past lives");
+// console.log(movieDatabase);
 
 // 2
-movieDatabase.addRating = function (movieTitle, rating) {
-for (let i = 0; i < movies.length; i++) {
-    if (movies[i].ratings) {
-        
+function addRating(movieTitle, rating) {
+  for (let i = 0; i < movieDatabase.movies.length; i++) {
+    if (movieDatabase.movies[i].title === movieTitle) {
+      movieDatabase.movies[i].ratings.push(rating);
     }
-    
+  }
 }
-}
+movieDatabase.addRating = addRating;
+movieDatabase.addRating("portrait of a lady on fire", 8);
+movieDatabase.addRating("portrait of a lady on fire", 7);
+movieDatabase.addRating("portrait of a lady on fire", 10);
+movieDatabase.addRating("portrait of a lady on fire", 9);
 
-const str = "rwaa"
-const obj = {}
-function strCount (name) {
-for (let i = 0; i < str.length; i++) {
- if (obj[str[i]] != [i]) {
-    obj[str[i]] = 1
- }
-    else {
-        obj[str[i]]++;
-    }
-    if (obj.hasOwnProperty) {
-        
-    }
-}
+movieDatabase.addRating("decision to leave", 9.5);
+movieDatabase.addRating("decision to leave", 9);
+movieDatabase.addRating("decision to leave", 7.5);
+movieDatabase.addRating("decision to leave", 6.5);
 
+movieDatabase.addRating("lady bird", 7);
+movieDatabase.addRating("lady bird", 8);
+movieDatabase.addRating("lady bird", 9);
+movieDatabase.addRating("lady bird", 9.7);
+console.log(movieDatabase.movies);
+
+// function removeRating(movieTitle, rating) {
+//   const idx = movieDatabase.movies.ratings.indexOf(rating)
+//   for (let i = 0; i < movieDatabase.movies.length; i++) {
+//     if (movieDatabase.movies[i].title === movieTitle) {
+//       movieDatabase.movies[i].ratings.splice(idx,1);
+//     }
+//   }
+// }
+// movieDatabase.removeRating = removeRating;
+// movieDatabase.removeRating("decision to leave", 9.5);
+// console.log(movieDatabase.movies);
+
+// 3
+function calculateAverageRating(movieTitle) {
+  let sum = 0;
+  count = 0;
+  for (let i = 0; i < movieDatabase.movies.length; i++) {
+    if (movieDatabase.movies[i].title === movieTitle) {
+      for (let j = 0; j < movieDatabase.movies[i].ratings.length; j++) {
+        sum += movieDatabase.movies[i].ratings[j];
+        count++;
+      }
+      movieDatabase.movies[i].averageRating = sum / count;
+      // console.log(movieDatabase.movies[i].averageRating);
+      break;
+    }
+  }
 }
-strCount(str)
-console.log(obj);
+movieDatabase.calculateAverageRating = calculateAverageRating;
+movieDatabase.calculateAverageRating("decision to leave");
+movieDatabase.calculateAverageRating("portrait of a lady on fire");
+movieDatabase.calculateAverageRating("lady bird");
+console.log(movieDatabase.movies);
+
+// 4
+function searchMovie(str) {
+  const result = movieDatabase.movies.filter((movie) => {
+    const title = movie.title;
+    return title.includes(str);
+  });
+  console.log(result);
+}
+movieDatabase.searchMovie = searchMovie;
+movieDatabase.searchMovie("lady");
+
+// 5
+function sortMoviesByRating() {
+   movieDatabase.movies.sort((a,b) => b.averageRating - a.averageRating)
+  //  for (let i = 0; i < movieDatabase.movies.length; i++) {
+  //    if (movieDatabase.movies[i].) {
+      
+  //    }
+  //  }
+  console.log(movieDatabase.movies);
+}
+movieDatabase.sortMoviesByRating = sortMoviesByRating
+movieDatabase.sortMoviesByRating()
+
+
+function sortMoviesByTitle() {
+   movieDatabase.movies.sort((a,b) => {
+     const titleA = a.title.toUpperCase()
+     const titleB = b.title.toUpperCase()
+    if (titleA < titleB) {
+      return -1
+    }
+    if (titleA > titleB) {
+      return 1
+    }
+    return 0
+  })
+ 
+  console.log(movieDatabase.movies);
+}
+movieDatabase.sortMoviesByTitle = sortMoviesByTitle
+movieDatabase.sortMoviesByTitle()
+
