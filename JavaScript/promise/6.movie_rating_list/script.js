555fe1a7c20354891cd2278ab68699b42d882e96
Rwaa Assi <rwaaassi123@gmail.com>
2024-05-21 13:36:11 +0300

    movie rating list

--- JavaScript/promise/6.movie_rating_list/script.js
+++ JavaScript/promise/6.movie_rating_list/script.js
@@ -9,74 +9,74 @@ class Movie {
     return this.title;
   }
   getReleaseDate() {
-    // let month = this.releaseDate.slice(5,7)
-    // const monthNames = [
-    //   "January",
-    //   "February",
-    //   "March",
-    //   "April",
-    //   "May",
-    //   "June",
-    //   "July",
-    //   "August",
-    //   "September",
-    //   "October",
-    //   "November",
-    //   "December",
-    // ];
-    // let monthName = monthNames[parseInt(month) -1];
-    //  let localizedDate =
-    //    this.releaseDate.slice(0, 5) + monthName + this.releaseDate.slice(7);
-    //    return localizedDate;
-
-    let month = this.releaseDate.charAt(6) + this.releaseDate.charAt(7);
-
-    switch (month) {
-      case "01":
-        month = "January";
-        break;
-      case "02":
-        month = "February";
-        break;
-      case "03":
-        month = "March";
-        break;
-      case "04":
-        month = "April";
-        break;
-      case "05":
-        month = "May";
-        break;
-      case "06":
-        month = "June";
-        break;
-      case "07":
-        month = "July";
-        break;
-      case "08":
-        month = "August";
-        break;
-      case "09":
-        month = "September";
-        break;
-      case "10":
-        month = "October";
-        break;
-      case "11":
-        month = "November";
-        break;
-      case "12":
-        month = "December";
-        break;
-
-      default:
-        month = "null";
-        break;
-    }
+    let month = this.releaseDate.slice(5, 7);
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let monthName = monthNames[parseInt(month) - 1];
     let localizedDate =
-      this.releaseDate.slice(0, 5) + month + this.releaseDate.slice(7);
-
+      this.releaseDate.slice(0, 5) + monthName + this.releaseDate.slice(7);
     return localizedDate;
+
+    // let month = this.releaseDate.charAt(6) + this.releaseDate.charAt(7);
+
+    // switch (month) {
+    //   case "01":
+    //     month = "January";
+    //     break;
+    //   case "02":
+    //     month = "February";
+    //     break;
+    //   case "03":
+    //     month = "March";
+    //     break;
+    //   case "04":
+    //     month = "April";
+    //     break;
+    //   case "05":
+    //     month = "May";
+    //     break;
+    //   case "06":
+    //     month = "June";
+    //     break;
+    //   case "07":
+    //     month = "July";
+    //     break;
+    //   case "08":
+    //     month = "August";
+    //     break;
+    //   case "09":
+    //     month = "September";
+    //     break;
+    //   case "10":
+    //     month = "October";
+    //     break;
+    //   case "11":
+    //     month = "November";
+    //     break;
+    //   case "12":
+    //     month = "December";
+    //     break;
+
+    //   default:
+    //     month = "null";
+    //     break;
+    // }
+    // let localizedDate =
+    //   this.releaseDate.slice(0, 5) + month + this.releaseDate.slice(7);
+
+    // return localizedDate;
   }
   getPicture() {
     return this.picture;
@@ -90,7 +90,8 @@ const getHttpOptions = () => ({
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer your-bearer-token-here",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzg1Y2Y5NDYyOWU3MDhlZmY4MDQyZjEyYjI1MWUxYSIsInN1YiI6IjY2NGM3NGM2ZmNhYjNiNWI1MTc5Yjc5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TFXBrggzNUd1yPLGHTROWovxWel8Ucb9ZhzyvMez-a8",
   },
 });
 
