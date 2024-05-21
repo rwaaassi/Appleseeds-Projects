9df78a82a2b55164738ecaeb250076fbb99afda8
Rwaa Assi <rwaaassi123@gmail.com>
2024-05-21 17:27:51 +0300

    destructuring

--- JavaScript/array_methods/spread_destructuring.js
+++ JavaScript/array_methods/spread_destructuring.js
@@ -15,29 +15,30 @@ let topMovies = [
   "The Dark Knight",
   "12 Angry Men",
 ];
-let [TheShawshankRedemption, TheGodfather, TheDarkKnight, AngryMen] =
-  (topMovies[(TheShawshankRedemption, TheGodfather, TheDarkKnight, AngryMen)] =
-    [TheGodfather, TheShawshankRedemption, TheDarkKnight, AngryMen]);
-console.log(TheShawshankRedemption, TheGodfather);
+[topMovies[0], topMovies[1]] = [topMovies[1], topMovies[0]];
+console.log(topMovies);
 
 // 3
 const movieRatings = {
   "The Matrix": 8.7,
   Inception: 8.8,
 };
-
 const updates = {
   Inception: 9.0,
   "The Matrix": 9.1,
 };
+function updateRatings(movieRatings, updates) {
+  const newRatings = { ...movieRatings, ...updates };
+  console.log(newRatings);
+}
+updateRatings(movieRatings, updates);
 
 // 4
 const list1 = ["The Shawshank Redemption", "The Godfather", "The Dark Knight"];
 const list2 = ["Forrest Gump", "Inception", "The Godfather"];
 const combined = [...list1, ...list2];
-if (list1) {
-}
-console.log(combined);
+
+console.log(...new Set(combined));
 
 // 5
 const movieDetails1 = {
@@ -48,9 +49,11 @@ const movieDetails1 = {
     third: "Ellen Page",
   },
 };
-const { movieTitle, cast } = movieDetails;
-
-console.log();
+const {
+  titlee,
+  cast: { first, second },
+} = movieDetails1;
+console.log(first, second);
 
 // 6
 const baseInfo = {
@@ -62,6 +65,8 @@ const additionalInfo = {
   year: 2010,
   rating: 8.8,
 };
+const merged = { ...baseInfo, ...additionalInfo };
+console.log(merged);
 
 // 7
 const movieDetails2 = {
@@ -70,6 +75,11 @@ const movieDetails2 = {
   rating: 9.0,
   genres: ["Action", "Adventure", "Sci-Fi"],
 };
+movieDetails2.genres.push("Drama", "Action");
+
+const arr = movieDetails2.genres;
+
+console.log(...new Set(arr));
 
 // 8
 const watchlist = [
@@ -77,16 +87,38 @@ const watchlist = [
   ["Inception", "The Dark Knight"],
   ["The Dark Knight", "Interstellar"],
 ];
+const month1 = watchlist[0];
+const month2 = watchlist[1];
+const month3 = watchlist[2];
+const movieTitles = [...month1, ...month2, ...month3];
+const arrOfMovieTitles = [...new Set(movieTitles)];
 
+console.log(arrOfMovieTitles);
 // 9
 const movies = [
   { title: "The Matrix", rating: 8.7 },
   { title: "Inception", rating: 8.8 },
 ];
-
+function increaseRating(movieTitle, newRating) {
+  return movies.map((movie) => {
+    if (movie.title === movieTitle) {
+      return { ...movie, rating: newRating };
+    }
+    return movie;
+  });
+}
+console.log(increaseRating("Inception", 10));
 // 10
 const movieDetails3 = {
   title: "The Dark Knight",
   director: "Christopher Nolan",
   cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
 };
+function newMovieObj(movieDetails) {
+  const { title, cast } = movieDetails;
+  return cast.map((actor) => {
+    const [firstName] = actor.split(" ");
+    return { title, firstName };
+  });
+}
+console.log(newMovieObj(movieDetails3));
--- test.js
+++ test.js
@@ -2,3 +2,12 @@ const releaseDate = "2023-07-19"
 const month = releaseDate.charAt(5) + releaseDate.charAt(6)
 console.log(month);
 console.log(typeof month);
+
+const movieRatings = {
+  "The Matrix": 8.7,
+  Inception: 8.8,
+};
+function spread(movieRatings) {
+    return movieRatings
+}
+spread(...movieRatings)
